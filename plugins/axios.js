f565c7638f660c0fd5796daf90c8dda0538233f8
Higemura <higemura.mustache@gmail.com>
2018-06-04 18:22:45 +0900

    axios 追加

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,8 @@
 export default function ({ $axios }) {
   $axios.onRequest( (config) => {
     if (config.url.indexOf('api.github.com') +1 ) {
-      config.headers.Authorization = `token XXXXXXXXX`
+      config.headers.Authorization = `token f7f72ae5049c904adbd4e120c83a8e0becb5916f`
+      // XXXX is your access token
     }
   })
 }
