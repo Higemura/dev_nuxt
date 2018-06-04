4495c9fa57c29fe1dc2a4cd0279b586d21d972c3
Higemura <higemura.mustache@gmail.com>
2018-06-04 17:17:04 +0900

    ファーストコミット

---
+++ pages/users/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>/users/about.vue</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ pages/users/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>/users/index.vue</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
