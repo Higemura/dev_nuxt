b43265410479ab75ed6b006e43a23c141505ae5c
Higemura <higemura.mustache@gmail.com>
2018-06-05 08:07:15 +0900

    コメント削除

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,6 @@ export default function ({ $axios }) {
   $axios.onRequest( (config) => {
     if (config.url.indexOf('api.github.com') +1 ) {
       config.headers.Authorization = `f7f72ae5049c904adbd4e120c83a8e0becb5916f`
-      // XXXX is your access token
     }
   })
 }
