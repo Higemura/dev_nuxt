e90b3b99f09a129a0f45c055eece03cf55e617e6
Higemura <higemura.mustache@gmail.com>
2018-06-13 15:25:23 +0900

    差分戻し

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1>{{ user.name }}</h1>
-    <img :src="user.avatar_url">
+    <h1>{{ id }}</h1>
+    <!-- <img :src="user.avatar_url"> -->
   </div>
 </template>
 <script>
 export default {
-  async asyncData ({ params, app }) {
-    const { id } = params
-    const user = await app.$axios.$get(
-      `https://api.github.com/users/${id}`
-    )
-    return { user }
+  async asyncData({ params }) {
+    const { id } = params;
+    // const user = await app.$axios.$get(
+    //   `https://api.github.com/users/${id}`
+    // )
+    return { id };
   }
-}
+};
 </script>
