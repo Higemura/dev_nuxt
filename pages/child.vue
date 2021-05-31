c5ba53cb020b7baaf2dd513a53394fd21f988cde
Higemura <higemura.mustache@gmail.com>
2021-05-31 15:30:45 +0900

    全削除

--- pages/child.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>child</h1>
-  </div>
-</template>
--- plugins/axios.js
+++
@@ -1,8 +0,0 @@
-export default function ({ $axios }) {
-  $axios.onRequest( (config) => {
-    if (config.url.indexOf('api.github.com') +1 ) {
-      config.headers.Authorization = `token f7f72ae5049c904adbd4e120c83a8e0becb5916f`
-      // XXXX is your access token
-    }
-  })
-}
